0a92b403c6777841b3275747b161909988d78aa6
Risa Orsinger <risa.orsinger@gmail.com>
2019-01-05 11:55:03 -0600

    update readme with current codebat status

--- ex33.py
+++ ex33.py
@@ -1,16 +1,19 @@
-i = 0
-numbers = []
 
-while i < 6:
-	print(f"At the top, i is {i}")
-	numbers.append(i)
+def while_loop(until):
+	i = 0
+	numbers = []
+	while i < until:
+		print(f"At the top, i is {i}")
+		numbers.append(i)
 
-	i += 1
+		i += 1
 
-	print("Numbers now: ", numbers)
-	print(f"At the bottom, i is {i}")
+		print("Numbers now: ", numbers)
+		print(f"At the bottom, i is {i}")
 
-print("The numbers:  ")
+	print("The numbers:  ")
 
-for num in numbers:
-	print(num)
+	for num in numbers:
+		print(num)
+
+while_loop(9)
