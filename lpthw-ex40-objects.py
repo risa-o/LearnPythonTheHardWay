d724977b84c69ce576a6bc628302cf0b7968bf0e
risa-o <risa.orsinger@gmail.com>
2019-01-29 11:38:46 -0600

    added study drills to ex40

--- lpthw-ex40-objects.py
+++ lpthw-ex40-objects.py
@@ -1,7 +1,8 @@
 class Song(object):
 
-	def __init__(self, lyrics):
+	def __init__(self, lyrics, artist):
 		self.lyrics = lyrics
+		self.artist = artist
 
 	def sing_me_a_song(self):
 		for line in self.lyrics:
@@ -9,11 +10,22 @@ class Song(object):
 
 happy_bday = Song(["Happy birthday to you",
 				"I don't want to get sued",
-				"So I'll stop right there"])
+				"So I'll stop right there"],"?")
 
 bull_on_parade = Song(["They rally around tha family",
-						"With pockets full of shells"])
+						"With pockets full of shells"], "?")
+
+freshmen = Song(["For the life of me,",
+				"I cannot remember",
+				"what made us think we were wise",
+				"and would never compromise"],"The Verve Pipe")
 
 happy_bday.sing_me_a_song()
+freshmen.sing_me_a_song()
 
-bull_on_parade.sing_me_a_song()
+mest_lyrics = ["Top down, seat back,",
+				"Rollin in my cadillac"]
+
+mest = Song(mest_lyrics, "Mest")
+mest.sing_me_a_song()
+print(mest.artist)
