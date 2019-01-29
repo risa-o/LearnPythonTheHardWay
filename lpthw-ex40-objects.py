cccc7763b543f68dc6f11b25c122071d59e59608
risa-o <risa.orsinger@gmail.com>
2019-01-29 11:20:01 -0600

    updated to correct __init__ and variable name

--- lpthw-ex40-objects.py
+++ lpthw-ex40-objects.py
@@ -1,6 +1,6 @@
 class Song(object):
 
-	def _init_(self, lyrics):
+	def __init__(self, lyrics):
 		self.lyrics = lyrics
 
 	def sing_me_a_song(self):
@@ -8,12 +8,12 @@ class Song(object):
 			print(line)
 
 happy_bday = Song(["Happy birthday to you",
-	"I don't want to get sued",
-	"So I'll stop right there"])
+				"I don't want to get sued",
+				"So I'll stop right there"])
 
 bull_on_parade = Song(["They rally around tha family",
 						"With pockets full of shells"])
 
 happy_bday.sing_me_a_song()
 
-bulls_on_parade.sing_me_a_song()
+bull_on_parade.sing_me_a_song()
